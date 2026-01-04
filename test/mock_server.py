7f3bcac2f1d74f356c813c6610f88cba933c741c
hylom <hylom@hylom.net>
2026-01-05 01:22:15 +0900

    add mock_server
    This mock_server (`test/mock_server.py`) works as mock of OpenAI
    compatible API server for test.

---
+++ test/mock_server.py
@@ -0,0 +1,65 @@
+import http.server
+import json
+
+class HTTPRequestHandler(http.server.BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == "/v1/chat/completions":
+            self.send_error(405)
+            return
+        self.send_error(404)
+
+    def do_POST(self):
+        # content type must be "application/json"
+        print("post")
+        if not self.headers.get_content_type().startswith("application/json"):
+            self.send_error(400)
+            return
+
+        if self.path == "/v1/chat/completions":
+            self.do_completions()
+            return
+        self.send_error(404)
+
+    def do_completions(self):
+        try:
+            length = int(self.headers.get("Content-Length", 0))
+        except ValueError:
+            self.send_error(400)
+            return
+        try:
+            body = json.loads(self.rfile.read(length))
+        except json.decoder.JSONDecodeError:
+            self.send_error(400)
+            return
+
+        msgs = body.get("messages", [])
+        if len(msgs) == 0:
+            self.send_error(400)
+            return
+
+        content = msgs[-1].get("content", "")
+        resp = {
+            "choices": [{
+                "index": 0,
+                "message": {
+                    "role": "assistant",
+                    "reasoning_content": content,
+                    "content": f"result:\n```\n{content}\n```"
+                    },
+                }],
+            }
+        resp_json = json.dumps(resp)
+        resp_body = resp_json.encode("utf8")
+        self.send_response(200)
+        self.send_header("content-type", "application/json")
+        self.send_header("content-length", str(len(resp_body)))
+        self.end_headers()
+        self.wfile.write(resp_body)
+
+def start_server(host="", port=8080):
+    server_address = (host, port)
+    httpd = http.server.HTTPServer(server_address, HTTPRequestHandler)
+    httpd.serve_forever()
+
+if __name__ == "__main__":
+    start_server()
